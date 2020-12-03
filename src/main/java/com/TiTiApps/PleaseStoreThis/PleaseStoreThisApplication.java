e21425c475d45c21b618065bd0cae90901f8eb19
Curcuma506 <jeanmichel.april@gmail.com>
2020-12-02 23:02:20 -0500

    Ajout de la BD

--- src/main/java/com/TiTiApps/PleaseStoreThis/PleaseStoreThisApplication.java
+++ src/main/java/com/TiTiApps/PleaseStoreThis/PleaseStoreThisApplication.java
@@ -3,14 +3,89 @@ package com.TiTiApps.PleaseStoreThis;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Iterator;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
 @SpringBootApplication
 public class PleaseStoreThisApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PleaseStoreThisApplication.class, args);
 
-		System.out.println("\n\n********************\nyo yo");
-		System.out.println("\n********************\nméga muscles!\n");
+		String filePath = "EmployeeData.xlsx";
+
+		try {
+
+            FileInputStream excelFile = new FileInputStream(new File(filePath));
+            Workbook workbook = new XSSFWorkbook(excelFile);
+            Sheet datatypeSheet = workbook.getSheetAt(0);
+			Iterator<Row> iterator = datatypeSheet.iterator();
+			
+			System.out.println();
+
+            while (iterator.hasNext()) {
+
+                Row currentRow = iterator.next();
+				Iterator<Cell> cellIterator = currentRow.iterator();
+
+                while (cellIterator.hasNext()) {
+
+                    Cell currentCell = cellIterator.next();
+                    //getCellTypeEnum shown as deprecated for version 3.15
+                    //getCellTypeEnum ill be renamed to getCellType starting from version 4.0
+                    if (currentCell.getCellTypeEnum() == CellType.STRING) {
+                        System.out.print(currentCell.getStringCellValue() + "--");
+                    } else if (currentCell.getCellTypeEnum() == CellType.NUMERIC) {
+                        System.out.print(currentCell.getNumericCellValue() + "--");
+                    }
+
+                }
+                System.out.println();
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            Class.forName("org.hsqldb.jdbc.JDBCDriver" );
+        } catch (Exception e) {
+            System.err.println("ERROR: failed to load HSQLDB JDBC driver.");
+            e.printStackTrace();
+            return;
+        }
+       
+        Connection con = null;
+      
+        try {
+            //Registering the HSQLDB JDBC driver
+            Class.forName("org.hsqldb.jdbc.JDBCDriver");
+            //Creating the connection with HSQLDB
+            con = DriverManager.getConnection("jdbc:hsqldb:hsql://localhost/excel-tables-db", "SA", "");
+            
+            if (con!= null){
+                System.out.println("Connection created successfully");
+                
+            }else{
+                System.out.println("Problem with creating connection");
+            }
+        
+        }  catch (Exception e) {
+            e.printStackTrace(System.out);
+        }
+
 	}
 
 }
